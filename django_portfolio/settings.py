1c513e144e186f3bf84e2ae91436aa8f497ee414
juan felipe rodrigez granados <ing.juanfeliperodriguez@gmail.com>
2024-04-07 19:19:42 -0500

    octavo_commit

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['django-portfolio-wyeq.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 
