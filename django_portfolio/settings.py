32011e852b552a0bc70fc04828f146b65980aeac
Juan Felipe Rodriguez <133280694+felipe97rg@users.noreply.github.com>
2024-12-16 18:10:09 -0500

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -90,9 +90,8 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get("DATABASE_URL")
-if database_url:
-    DATABASES['default'] = dj_database_url.parse(database_url)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
